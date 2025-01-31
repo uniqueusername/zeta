33bd02564817bb2b202fb609ffa9180ca024410b
ary <ary@hyperbeam.sh>
2025-01-31 15:49:51 -0800

    fix camera movement while dashing

--- objects/player/camera_3d.gd
+++ objects/player/camera_3d.gd
@@ -1,6 +1,6 @@
 extends Camera3D
 
-const SENSITIVITY: float = 0.003;
+const SENSITIVITY: float = 0.001;
 const AUTO_ROTATE_RATE: float = 0.01
 const DASH_ROTATION: float = 0.02
 
@@ -29,11 +29,7 @@ func _rotate_to_base() -> void:
 
 func dash(dash_dir: float) -> void:
 	var tween: Tween = self.create_tween()
-	var new_rot: Vector3 = rotation
-	new_rot.z = DASH_ROTATION * dash_dir
-	tween.tween_property(self, "rotation", new_rot, 0.1)
-	
-	new_rot.z = 0
+	tween.tween_property(self, "rotation:z", DASH_ROTATION * dash_dir, 0.1)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self, "rotation", new_rot, 0.3)
+	tween.tween_property(self, "rotation:z", 0, 0.3)

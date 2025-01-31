947d503f9a3d313fdeb5ff1ae5600085ba320812
ary <7274791+uniqueusername@users.noreply.github.com>
2025-01-31 15:39:06 -0800

    add horizontal dash

--- objects/player/camera_3d.gd
+++ objects/player/camera_3d.gd
@@ -1,6 +1,8 @@
 extends Camera3D
 
 const SENSITIVITY: float = 0.003;
+const AUTO_ROTATE_RATE: float = 0.01
+const DASH_ROTATION: float = 0.02
 
 func _ready() -> void:
 	$AnimationPlayer.current_animation = "bob"
@@ -17,3 +19,21 @@ func _on_movement_walking() -> void:
 
 func _on_movement_not_walking() -> void:
 	$AnimationPlayer.stop()
+	
+func _rotate_to_base() -> void:
+	var new_rot: Vector3 = rotation
+	new_rot.z = 0
+	
+	var tween: Tween = self.create_tween()
+
+
+func dash(dash_dir: float) -> void:
+	var tween: Tween = self.create_tween()
+	var new_rot: Vector3 = rotation
+	new_rot.z = DASH_ROTATION * dash_dir
+	tween.tween_property(self, "rotation", new_rot, 0.1)
+	
+	new_rot.z = 0
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "rotation", new_rot, 0.3)
--- objects/player/movement.gd
+++ objects/player/movement.gd
@@ -1,89 +1,72 @@
 extends Node3D
 
+# signals
 signal walking
 signal not_walking
 
+# references
 @export var p: CharacterBody3D
 @export var camera: Camera3D
+@export var speed_label: Label
 
-@export var max_speed: float = 15
-@export var start_rate: float = 0.5
-@export var stop_rate: float = 1 # aka friction
+# constants
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity") * 4
-var slide_height: float = 0.7
-var slide_speed_threshold: float = 10
-var slide_height_rate: float = 1
-var slide_accel_multiplier: float = 0.5
-var slide_stop_rate_multiplier: float = 0.1
-var jump_velocity: float = 8
+## walking
+const walk_accel: float = 150
+const max_walk_vel: float = 15
 
-var sliding: bool = false
+## other
+const jump_vel: float = 3
+const dash_vel: float = 25
 
-func _physics_process(delta: float) -> void:
-	handle_gravity(delta)
+func _physics_process(delta) -> void:
+	var input_dir: Vector2 = Input.get_vector("left", "right", "backward", "forward")
+	var movement_dir: Vector2 = _calculate_movement_dir(input_dir)
 	
-	if (p.velocity.length() < slide_speed_threshold): stop_slide()
-	if sliding: handle_movement_uncapped(delta)
-	else: handle_movement(delta)
-	
+	if not p.is_on_floor(): _apply_gravity(delta)
+	_handle_walking(movement_dir, delta)
 	p.move_and_slide()
+	
+	_update_speed_label()
 
-func handle_gravity(delta: float):
-	if not p.is_on_floor():
-		p.velocity.y -= gravity * delta
-
-func handle_movement(delta: float) -> void:
-	var camera_rotation: float = fmod(-camera.rotation.y, 2*PI)
-	var input_dir: Vector2 = Input.get_vector("left", "right", "forward", "backward")
-	input_dir = input_dir.rotated(camera_rotation)
-	
-	var curr_velocity: Vector2 = Vector2(p.velocity.x, p.velocity.z)
-	var velocity_target: Vector2 = input_dir * max_speed
-	var new_velocity: Vector2
-	var super_fast_multiplier: float = slide_stop_rate_multiplier if curr_velocity.length() > max_speed else 1
-	if velocity_target:
-		new_velocity = curr_velocity.move_toward(velocity_target, start_rate * 10 * super_fast_multiplier)
-		walking.emit()
-	else:
-		new_velocity = curr_velocity.move_toward(Vector2.ZERO, stop_rate * super_fast_multiplier)
-		not_walking.emit()
-	
-	p.velocity.x = new_velocity.x
-	p.velocity.z = new_velocity.y
-	
-func handle_movement_uncapped(delta: float) -> void:
-	var camera_rotation: float = fmod(-camera.rotation.y, 2*PI)
-	var input_dir: Vector2 = Input.get_vector("left", "right", "forward", "backward")
-	input_dir = input_dir.rotated(camera_rotation)
-	
-	var curr_velocity: Vector2 = Vector2(p.velocity.x, p.velocity.z)
-	var new_velocity: Vector2 = (curr_velocity + input_dir * start_rate * slide_accel_multiplier)
-	var angle_btwn_vel: float = curr_velocity.angle_to(new_velocity)
-	new_velocity = new_velocity.rotated(-angle_btwn_vel * 0.5)
-	
-	p.velocity.x = new_velocity.x
-	p.velocity.z = new_velocity.y
-	
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("slide") and p.velocity.length() > slide_speed_threshold:
-		start_slide()
-	elif event.is_action_released("slide"):
-		stop_slide()
-		
 	if event.is_action_pressed("jump") and p.is_on_floor():
-		jump()
+		_jump()
+		
+	if event.is_action_pressed("dash"):
+		_dash(Input.get_axis("left", "right"))
+
+func _apply_gravity(delta: float) -> void:
+	p.velocity.y -= gravity * delta
+
+func _handle_walking(movement_dir: Vector2, delta: float) -> void:
+	var curr_vel: Vector2 = _get_2d_player_vel()
 	
-func start_slide() -> void:
-	if not sliding:
-		$AnimationPlayer.current_animation = "slide"
-		sliding = true
+	if movement_dir:
+		walking.emit()
+		_set_player_vel_from_2d(curr_vel.move_toward(movement_dir * max_walk_vel, walk_accel * delta))
+	else:
 		not_walking.emit()
+		_set_player_vel_from_2d(curr_vel.move_toward(Vector2.ZERO, walk_accel * delta))
+
+func _jump():
+	p.velocity.y += jump_vel
 	
-func stop_slide() -> void:
-	if sliding:
-		$AnimationPlayer.current_animation = "unslide"
-		sliding = false
-	
-func jump() -> void:
-	p.velocity.y += jump_velocity
+func _dash(dash_dir: float):
+	var input: Vector2 = Vector2(dash_dir, 0)
+	var new_vel: Vector2 = _get_2d_player_vel() + _calculate_movement_dir(input) * dash_vel
+	_set_player_vel_from_2d(new_vel)
+	camera.dash(dash_dir)
+
+func _update_speed_label():
+	if speed_label: speed_label.text = str(int(_get_2d_player_vel().length()))
+
+func _get_2d_player_vel() -> Vector2:
+	return Vector2(p.velocity.x, -p.velocity.z)
+
+func _set_player_vel_from_2d(vel: Vector2) -> void:
+	p.velocity = Vector3(vel.x, p.velocity.y, -vel.y)
+
+func _calculate_movement_dir(input: Vector2) -> Vector2:
+	return input.rotated(camera.rotation.y)

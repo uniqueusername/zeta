a0555f8043021353457786c91c6abbc7de745c5f
ary <ary@hyperbeam.sh>
2025-03-03 00:31:39 -0800

    all dialogue...

--- manager.gd
+++ manager.gd
@@ -1,8 +1,31 @@
 extends Node
 
+var dialogue_array: Array = []
+var curr_dialogue: int = 0
+var curr_dialogue_box
+
+func _ready() -> void:
+	var dir = DirAccess.open("res://objects/dialogue/dialogues/")
+	if dir:
+		dir.list_dir_begin()
+		var file = dir.get_next()
+		while file:
+			dialogue_array.append(ResourceLoader.load(dir.get_current_dir() + "/" + file))
+			file = dir.get_next()
+	print(dialogue_array)
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("reset"):
 		get_tree().reload_current_scene()
 		
 	if event.is_action_pressed("exit"):
 		get_tree().quit()
+		
+	if event.is_action_pressed("parry"):
+		show_next_dialogue()
+		
+func show_next_dialogue():
+	if curr_dialogue_box != null: curr_dialogue_box.queue_free()
+	curr_dialogue_box = dialogue_array[curr_dialogue].instantiate()
+	add_child(curr_dialogue_box)
+	curr_dialogue += 1
--- objects/dialogue/dialogue.gd
+++ objects/dialogue/dialogue.gd
@@ -1,8 +1,18 @@
 extends MarginContainer
 
-@export var text: String
-@export var image: Texture2D
+@onready var label: Label = $MarginContainer/HBoxContainer/ColorRect/MarginContainer/ColorRect/MarginContainer/ColorRect/MarginContainer/Label
+@onready var timer: Timer = $Timer
 
 func _ready() -> void:
-	if text: $MarginContainer/HBoxContainer/ColorRect/MarginContainer/ColorRect/MarginContainer/ColorRect/MarginContainer/Label.text = text
-	if image: $MarginContainer/HBoxContainer/TextureRect.texture = image
+	label.visible_ratio = 0
+	
+func _process(delta: float) -> void:
+	label.visible_ratio += delta / label.text.length() * 25
+	if label.visible_ratio == 1: timer.stop()
+
+func _on_timer_timeout() -> void:
+	$AudioStreamPlayer2D.play()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("attack") and label.visible_ratio < 1: label.visible_ratio = 1
+	elif event.is_action_pressed("attack") and label.visible_ratio == 1: queue_free()
